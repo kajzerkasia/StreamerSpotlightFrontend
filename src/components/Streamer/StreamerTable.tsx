4121791d6132a9574286591c1a0bdce4b737fc8e
Kasia <kajzerkatarzyna1@gmail.com>
2023-06-24 12:46:06 +0200

    Created components StreamerForm.tsx and StreamerTable.tsx.

--- src/components/Streamer/StreamerTable.tsx
+++ src/components/Streamer/StreamerTable.tsx
@@ -1,9 +1,187 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {StreamerEntity, Status} from 'types';
+import {TbQuestionMark, TbX, TbHeartbeat, TbDotsVertical} from "react-icons/tb";
+import {IconContext} from "react-icons";
+import {Link} from "react-router-dom";
+import {apiUrl} from "../../config/api";
+import './StreamerTable.css';
+import {StreamerForm} from "./StreamerForm";
 
 export const StreamerTable = () => {
+
+    const [streamersList, setStreamersList] = useState<StreamerEntity[]>([]);
+    const [confirmDeleteStreamer, setConfirmDeleteStreamer] = useState<boolean>(false);
+    const [streamerToDeleteId, setStreamerToDeleteId] = useState(null);
+
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        fetch(`${apiUrl}/api/streamer/streamers`, {
+            method: 'GET',
+            signal: abortController.signal
+        }).then(res => res.json())
+            .then((streamers) => {
+                setStreamersList(streamers)
+            })
+
+        return () => {
+            try {
+                abortController.abort()
+            } catch {
+            }
+        };
+    }, [])
+
+    const addStreamer = async (values: StreamerEntity) => {
+
+        const res = await fetch(`${apiUrl}/api/streamer/streamers`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+        })
+
+        const data = await res.json();
+
+        setStreamersList(list => [...list, data]);
+
+    };
+
+    const editStreamer = async (values: StreamerEntity) => {
+        const res = await fetch(`${apiUrl}/api/streamer/streamers/${values.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+        })
+        if (!res.ok) {
+            throw new Error('An error occurred while trying to update the streamer name.');
+        }
+        return await res.json();
+    };
+
+    const handleUpdateStreamer = (updatedStreamer: StreamerEntity) => {
+        setStreamersList((streamersList) =>
+            streamersList.map((streamer) =>
+                streamer.id === updatedStreamer.id ? updatedStreamer : streamer
+            )
+        );
+    };
+
+    const handleDeleteStreamer = async (streamerId: any) => {
+        setStreamerToDeleteId(streamerId);
+    };
+
+    const handleConfirmDelete = async () => {
+        const res = await fetch(
+            `${apiUrl}/api/streamer/streamers/${streamerToDeleteId}`,
+            {method: "DELETE"}
+        )
+        if ([400, 500].includes(res.status)) {
+            const error = await res.json();
+            alert(`An error occured: ${error.message}`);
+            return;
+        }
+        setStreamersList((streamersList) =>
+            streamersList.filter((streamer) => streamer.id !== streamerToDeleteId)
+        );
+        setConfirmDeleteStreamer(false);
+
+    };
+
+    const handleCancelDelete = () => {
+        setConfirmDeleteStreamer(false);
+        setStreamerToDeleteId(null);
+    };
+
     return (
-        <div>
-            <p>Streamers</p>
+        <div className="div_streamers_container">
+            <IconContext.Provider value={{className: 'react_icon_logo'}}>
+                <h1 className="main-h1"><TbHeartbeat/>Streamer Spotlight App</h1>
+            </IconContext.Provider>
+
+            <div className="div_streamers_table_container">
+                <table className="streamers_table">
+
+                    <thead>
+                    <tr>
+                        <td className="training-plans" align="center" colSpan={4}>
+                            <h1 className="h1_streamers"> Top Streamers</h1>
+                        </td>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    <tr>
+                        <td className="question-td">
+                            <IconContext.Provider value={{className: 'react-icons'}}>
+                                <Link to="/instruction"><TbQuestionMark/></Link>
+                            </IconContext.Provider>
+                        </td>
+                        <StreamerForm
+                            initialValues={{
+                                name: '',
+                                platform: '',
+                            }}
+                            onSubmit={async (values, reset) => {
+                                if (
+                                    values.name &&
+                                    values.platform
+                                ) {
+                                    await addStreamer(values);
+                                    reset();
+                                }
+
+                                // else {
+                                //     setInformationModalIsOpen(true);
+                                // }
+                            }}
+                            actionType={Status.Add}
+                        />
+                    </tr>
+
+                    {streamersList.map((streamer) => (
+                        <tr key={`${streamer.id}`}>
+                            <td>
+                                <IconContext.Provider value={{className: 'react-icons'}}>
+                                    <button onClick={() => handleDeleteStreamer(streamer.id)}><TbX/></button>
+                                </IconContext.Provider>
+                            </td>
+                            <StreamerForm
+                                initialValues={streamer}
+                                onSubmit={async (values) => {
+                                    if (values.name) {
+                                        await editStreamer(values);
+                                        await handleUpdateStreamer(values);
+                                    } else {
+                                        // setInformationModalIsOpen(true);
+                                        values.name = streamer.name;
+                                        values.platform = streamer.platform;
+                                    }
+                                }}
+                                actionType={Status.Save}
+                            />
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+            {/*<ConfirmationModal*/}
+            {/*    isOpen={confirmDeletePlan}*/}
+            {/*    onRequestClose={handleCancelDelete}*/}
+            {/*    onConfirm={handleConfirmDelete}*/}
+            {/*    onCancel={handleCancelDelete}*/}
+            {/*    text={text}*/}
+            {/*/>*/}
+            {/*<InformationModal*/}
+            {/*    isOpen={informationModalIsOpen}*/}
+            {/*    onRequestClose={closeModal}*/}
+            {/*    onConfirm={closeModal}*/}
+            {/*    onCancel={closeModal}*/}
+            {/*    text={textInformation}*/}
+            {/*/>*/}
         </div>
-    );
-};
+    )
+}
