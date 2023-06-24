70a0b93dd823c4c6a595c278264919753681867e
Kasia <kajzerkatarzyna1@gmail.com>
2023-06-24 20:55:47 +0200

    Added the function getPlatformIcon to Dropdown and StreamerForm to display icons next to names of streaming platforms.

--- src/components/Streamer/StreamerForm.tsx
+++ src/components/Streamer/StreamerForm.tsx
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
 import {StreamerEntity, Status} from 'types';
-import {TbPlus, TbCheck, TbBrandTwitch, TbBrandYoutube, TbBrandTiktok, TbBrandKick} from "react-icons/tb";
-import {IconContext} from "react-icons";
 import './StreamerForm.css';
 import {Dropdown, Option} from "../Dropdown/Dropdown";
 import {Votes} from "../Votes/Votes";
+import {getPlatformIcon} from "../../utils/getPlatformIcon";
 
 export type StreamerFormProps = {
     initialValues: StreamerEntity;
-    onSubmit: (values: StreamerEntity, reset: () => void) => void | Promise<void>;
+    onSubmit?: (values: StreamerEntity, reset: () => void) => void | Promise<void>;
     actionType: Status;
 };
 
@@ -42,36 +41,41 @@ export const StreamerForm = ({initialValues, onSubmit, actionType}: StreamerForm
 
     return (
         <>
-            <td className="td_streamer_name">
+            <td className="td_streamer_name" colSpan={1}>
                 {actionType === Status.Add ?
                     <label htmlFor="">
-                        Enter the streamer's name on the specified streaming platform.
+                        Enter the streamer's name on the specified streaming platform
                     </label>
                     : ''}
-                <input
-                    placeholder="What is the name of the streamer?"
-                    className="input_streamer_name"
-                    type="text"
-                    name="name"
-                    required
-                    value={values.name}
-                    onChange={(event) => handleChange('name', event.target.value)}
-                />
+                {actionType === Status.Add ?
+                    <input
+                        placeholder="What is the name of the streamer?"
+                        className="input_streamer_name"
+                        type="text"
+                        name="name"
+                        required
+                        value={values.name}
+                        onChange={(event) => handleChange('name', event.target.value)}
+                    />
+                    : <p>{values.name}</p>}
             </td>
-            <td className="td_streaming_platform">
+            <td className="td_streaming_platform" colSpan={1}>
                 {actionType === Status.Add ?
                     <label htmlFor="">
-                        Select the platform on which this streamer is broadcasting from the drop-down list.
+                        Select the platform this streamer is broadcasting on
                     </label>
                     : ''}
                 {actionType === Status.Add ?
                     <Dropdown placeholder="Select..." options={options} onItemClick={onItemClick}/>
-                    : <p>{values.platform}</p>}
+                    : (
+                        <div>
+                            {getPlatformIcon(values.platform)} {values.platform}
+                        </div>)}
             </td>
-            <td>
+            <td colSpan={1}>
                 {actionType === Status.Add ? (
 
-                    <button className="button" type='button' onClick={() => onSubmit(values, reset)}>Add streamer to the list
+                    <button className="button_add_streamer" type='button' onClick={() => onSubmit ? onSubmit(values, reset) : ''}>Add streamer
                     </button>
                 ) : (
                     <Votes/>
