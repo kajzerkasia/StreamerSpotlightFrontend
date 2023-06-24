6f459b360c7086ac7f18b3183478831500df6571
Kasia <kajzerkatarzyna1@gmail.com>
2023-06-24 18:39:04 +0200

    Updated component Votes.tsx and it's styles - Votes.css

--- src/components/Votes/Votes.tsx
+++ src/components/Votes/Votes.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {IconContext} from "react-icons";
+import {BiSolidLike, BiSolidDislike} from "react-icons/bi";
+import "./Votes.css";
 
 export const Votes = () => {
     return (
-        <div className="div_votes">
-            <p>Upvotes:</p>
-            <p>Downvotes:</p>
+        <div className="div_votes_container">
+            <IconContext.Provider value={{className: 'react_icon_like'}}>
+                <p><BiSolidLike/>321</p>
+            </IconContext.Provider>
+            <IconContext.Provider value={{className: 'react_icon_dislike'}}>
+                <p><BiSolidDislike/>123</p>
+            </IconContext.Provider>
         </div>
     );
 };
