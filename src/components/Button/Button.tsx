01665f43a01d1716f530be3b10005d80a67cbaf9
Kasia <kajzerkatarzyna1@gmail.com>
2023-06-26 15:27:49 +0200

    Removed one props from Button.tsx and one property in Button.css in .button class

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,21 +2,19 @@ import React, {HTMLAttributes, PropsWithChildren} from "react";
 import './Button.css';
 
 interface Props extends HTMLAttributes<HTMLButtonElement>, PropsWithChildren {
-    customClasses?: string;
     status?: "active" | "disabled";
 }
 
 export const Button = ({
                            className,
                            children,
-                           customClasses = "",
                            status = "active",
                            ...rest
                        }: Props) => {
     return (
         <button
             {...rest}
-            className={`button ${[status]} ${customClasses}`}
+            className={`button ${[status]}`}
         >
             {children}
         </button>
